a78ace4674237ee566513fea9420a0bdc903824b
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2024-06-18 12:52:05 -0300

    improving the debug print_opcode function

--- src/debug.c
+++ src/debug.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdint.h>
+
 #include "debug.h"
 
 #define clear() printf("\033[H\033[J")
 
-void update_clock_debug(Cpu8080* cpu) {
+void update_clock_debug(Cpu8080* cpu) 
+{
     clear();
     printf(" _______________ \n");
     printf("Current instruction:\n");
-    printf("\tasm: (work in progress)\n");
+    printf("\tasm: "), print_opcode(&cpu->rom[cpu->registers.pc]);
     printf("\tinteger: %u\n", (uint8_t)cpu->rom[cpu->registers.pc]);
     printf("\thex: 0x%0x\n", (uint8_t)cpu->rom[cpu->registers.pc]);
     printf(" _______________ \n");
@@ -26,11 +29,34 @@ void update_clock_debug(Cpu8080* cpu) {
     printf(" --------------- \n");
 }
 
-void start_clock_debug(Cpu8080* cpu) {
+void start_clock_debug(Cpu8080* cpu) 
+{
     clear();
     update_clock_debug(cpu);
 }
 
-void log_8080(const char *message) {
+void log_8080(const char *message) 
+{
     printf("8080 EMULATOR: %s", message);
+}
+
+void print_opcode(const uint8_t *opcode)
+{
+    //opcode table
+    switch (*opcode)
+    {
+        case 0x00:
+        case 0x08:
+        case 0x10:
+        case 0x11:
+        case 0x20:
+        case 0x21:
+        case 0x30:
+            printf("NOP\n");
+            break;
+
+        default:
+            printf("Instruction not implemented\n");
+            break;
+    }
 }
--- src/debug.h
+++ src/debug.h
@@ -6,5 +6,6 @@
 void update_clock_debug(Cpu8080* cpu);
 void start_clock_debug(Cpu8080* cpu);
 void log_8080(const char *message);
+void print_opcode(const uint8_t *opcode);
 
 #endif // DEBUG_H
