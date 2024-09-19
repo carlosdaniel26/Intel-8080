45acd430a49e65a18e62755933b626bcec030440
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2024-09-18 23:38:27 -0300

    improving sopcodes showing on debug

--- src/debug.c
+++ src/debug.c
@@ -359,6 +359,618 @@ void print_opcode(const uint8_t *opcode)
         case 0x67:
             printf("MOV H, A\n");
             break;
+
+        case 0x68:
+            printf("MOV L, B\n");
+            break;
+
+        case 0x69:
+            printf("MOV L, C\n");
+            break;
+
+        case 0x6A:
+            printf("MOV L, D\n");
+            break;
+        
+        
+        case 0x6B:
+	    printf("MOV L, E\n");
+	    break;
+
+	case 0x6C:
+	    printf("MOV L, H\n");
+	    break;
+
+	case 0x6D:
+	    printf("MOV L, L\n");
+	    break;
+
+	case 0x6E:
+	    printf("MOV L, M\n");
+	    break;
+
+	case 0x6F:
+	    printf("MOV A, L\n");
+	    break;
+
+	case 0x70:
+	    printf("MOV M, B\n");
+	    break;
+
+	case 0x71:
+	    printf("MOV M, C\n");
+	    break;
+
+	case 0x72:
+	    printf("MOV M, D\n");
+	    break;
+
+	case 0x73:
+	    printf("MOV M, E\n");
+	    break;
+
+	case 0x74:
+	    printf("MOV M, H\n");
+	    break;
+
+	case 0x75:
+	    printf("MOV M, L\n");
+	    break;
+
+	case 0x76:
+	    printf("HLT\n");
+	    break;
+
+	case 0x77:
+	    printf("MOV M, A\n");
+	    break;
+
+	case 0x78:
+	    printf("MOV A, B\n");
+	    break;
+
+	case 0x79:
+	    printf("MOV A, C\n");
+	    break;
+
+	case 0x7A:
+	    printf("MOV A, D\n");
+	    break;
+
+	case 0x7B:
+	    printf("MOV A, E\n");
+	    break;
+
+	case 0x7C:
+	    printf("MOV A, H\n");
+	    break;
+
+	case 0x7D:
+	    printf("MOV A, L\n");
+	    break;
+
+	case 0x7E:
+	    printf("MOV A, M\n");
+	    break;
+
+	case 0x7F:
+	    printf("MOV A, A\n");
+	    break;
+
+	case 0x80:
+	    printf("ADD B\n");
+	    break;
+
+	case 0x81:
+	    printf("ADD C\n");
+	    break;
+
+	case 0x82:
+	    printf("ADD D\n");
+	    break;
+
+	case 0x83:
+	    printf("ADD E\n");
+	    break;
+
+	case 0x84:
+	    printf("ADD H\n");
+	    break;
+
+	case 0x85:
+	    printf("ADD L\n");
+	    break;
+
+	case 0x86:
+	    printf("ADD M\n");
+	    break;
+
+	case 0x87:
+	    printf("ADD A\n");
+	    break;
+
+	case 0x88:
+	    printf("ADC B\n");
+	    break;
+
+	case 0x89:
+	    printf("ADC C\n");
+	    break;
+
+	case 0x8A:
+	    printf("ADC D\n");
+	    break;
+
+	case 0x8B:
+	    printf("ADC E\n");
+	    break;
+
+	case 0x8C:
+	    printf("ADC H\n");
+	    break;
+
+	case 0x8D:
+	    printf("ADC L\n");
+	    break;
+
+	case 0x8E:
+	    printf("ADC M\n");
+	    break;
+
+	case 0x8F:
+	    printf("ADC A\n");
+	    break;
+
+	case 0x90:
+	    printf("SUB B\n");
+	    break;
+
+	case 0x91:
+	    printf("SUB C\n");
+	    break;
+
+	case 0x92:
+	    printf("SUB D\n");
+	    break;
+
+	case 0x93:
+	    printf("SUB E\n");
+	    break;
+
+	case 0x94:
+	    printf("SUB H\n");
+	    break;
+
+	case 0x95:
+	    printf("SUB L\n");
+	    break;
+
+	case 0x96:
+	    printf("SUB M\n");
+	    break;
+
+	case 0x97:
+	    printf("SUB A\n");
+	    break;
+
+	case 0x98:
+	    printf("SBB B\n");
+	    break;
+
+	case 0x99:
+	    printf("SBB C\n");
+	    break; 
+	
+	case 0x9A:
+	    printf("SBB D\n");
+	    break;
+
+	case 0x9B:
+	    printf("SBB E\n");
+	    break;
+	
+	case 0x9C:
+	    printf("SBB H\n");
+	    break;
+
+	case 0x9D:
+	    printf("SBB L\n");
+	    break;
+
+	case 0x9E:
+	    printf("SBB M\n");
+	    break;
+
+	case 0x9F:
+	    printf("SBB A\n");
+	    break;
+
+	case 0xA0:
+	    printf("ANA B\n");
+	    break;
+
+	case 0xA1:
+	    printf("ANA C\n");
+	    break;
+
+	case 0xA2:
+	    printf("ANA D\n");
+	    break;
+
+	case 0xA3:
+	    printf("ANA E\n");
+	    break;
+
+	case 0xA4:
+	    printf("ANA H\n");
+	    break;
+
+	case 0xA5:
+	    printf("ANA L\n");
+	    break;
+
+	case 0xA6:
+	    printf("ANA M\n");
+	    break;
+
+	case 0xA7:
+	    printf("ANA A\n");
+	    break;
+
+	case 0xA8:
+	    printf("XRA B\n");
+	    break;
+
+	case 0xA9:
+	    printf("XRA C\n");
+	    break;
+
+	case 0xAA:
+	    printf("XRA D\n");
+	    break;
+
+	case 0xAB:
+	    printf("XRA E\n");
+	    break;
+
+	case 0xAC:
+	    printf("XRA H\n");
+	    break;
+
+	case 0xAD:
+	    printf("XRA L\n");
+	    break;
+
+	case 0xAE:
+	    printf("XRA M\n");
+	    break;
+
+	case 0xAF:
+	    printf("XRA A\n");
+	    break;
+
+	case 0xB0:
+	    printf("ORA B\n");
+	    break;
+
+	case 0xB1:
+	    printf("ORA C\n");
+	    break;
+
+	case 0xB2:
+	    printf("ORA D\n");
+	    break;
+
+	case 0xB3:
+	    printf("ORA E\n");
+	    break;
+
+	case 0xB4:
+	    printf("ORA H\n");
+	    break;
+
+	case 0xB5:
+	    printf("ORA L\n");
+	    break;
+
+	case 0xB6:
+	    printf("ORA M\n");
+	    break;
+
+	case 0xB7:
+	    printf("ORA A\n");
+	    break;
+
+	case 0xB8:
+	    printf("CMP B\n");
+	    break;
+
+	case 0xB9:
+	    printf("CMP C\n");
+	    break;
+
+	case 0xBA:
+	    printf("CMP D\n");
+	    break;
+
+	case 0xBB:
+	    printf("CMP E\n");
+	    break;
+
+	case 0xBC:
+	    printf("CMP H\n");
+	    break;
+
+	case 0xBD:
+	    printf("CMP L\n");
+	    break;
+
+	case 0xBE:
+	    printf("CMP M\n");
+	    break;
+
+	case 0xBF:
+	    printf("CMP A\n");
+	    break;
+
+	case 0xC0:
+	    printf("RNZ\n");
+	    break;
+
+	case 0xC1:
+	    printf("POP B\n");
+	    break;
+
+	case 0xC2:
+	    printf("JP NZ, addr\n");
+	    break;
+
+	case 0xC3:
+	    printf("JP addr\n");
+	    break;
+
+	case 0xC4:
+	    printf("CNZ addr\n");
+	    break;
+
+	case 0xC5:
+	    printf("PUSH B\n");
+	    break;
+
+	case 0xC6:
+	    printf("ADI data\n");
+	    break;
+
+	case 0xC7:
+	    printf("RST 0\n");
+	    break;
+
+	case 0xC8:
+	    printf("RZ\n");
+	    break;
+
+	case 0xC9:
+	    printf("RET\n");
+	    break;
+
+	case 0xCA:
+	    printf("JP Z, addr\n");
+	    break;
+
+	case 0xCB:
+	    printf("NOP\n");
+	    break;
+
+	case 0xCC:
+	    printf("CZ addr\n");
+	    break;
+
+	case 0xCD:
+	    printf("CALL addr\n");
+	    break;
+
+	case 0xCE:
+	    printf("ACI data\n");
+	    break;
+
+	case 0xCF:
+	    printf("RST 1\n");
+	    break;
+
+	case 0xD0:
+	    printf("RNC\n");
+	    break;
+
+	case 0xD1:
+	    printf("POP D\n");
+	    break;
+
+	case 0xD2:
+	    printf("JP NC, addr\n");
+	    break;
+
+	case 0xD3:
+	    printf("OUT port\n");
+	    break;
+
+	case 0xD4:
+	    printf("CNC addr\n");
+	    break;
+
+	case 0xD5:
+	    printf("PUSH D\n");
+	    break;
+
+	case 0xD6:
+	    printf("SUI data\n");
+	    break;
+
+	case 0xD7:
+	    printf("RST 2\n");
+	    break;
+
+	case 0xD8:
+	    printf("RC\n");
+	    break;
+
+	case 0xD9:
+	    printf("NOP\n");
+	    break;
+
+	case 0xDA:
+	    printf("JP C, addr\n");
+	    break;
+
+	case 0xDB:
+	    printf("IN port\n");
+	    break;
+
+	case 0xDC:
+	    printf("CC addr\n");
+	    break;
+
+	case 0xDD:
+	    printf("NOP\n");
+	    break;
+	
+	case 0xDE:
+	    printf("SBI data\n");
+	    break;
+
+	case 0xDF:
+	    printf("RST 3\n");
+	    break;
+
+	case 0xE0:
+	    printf("RPO\n");
+	    break;
+
+	case 0xE1:
+	    printf("POP H\n");
+	    break;
+
+	case 0xE2:
+	    printf("JP PO, addr\n");
+	    break;
+
+	case 0xE3:
+	    printf("XTHL\n");
+	    break;
+
+	case 0xE4:
+	    printf("CPO addr\n");
+	    break;
+
+	case 0xE5:
+	    printf("PUSH H\n");
+	    break;
+
+	case 0xE6:
+	    printf("ANI data\n");
+	    break;
+
+	case 0xE7:
+	    printf("RST 4\n");
+	    break;
+
+	case 0xE8:
+	    printf("RPE\n");
+	    break;
+
+	case 0xE9:
+	    printf("PCHL\n");
+	    break;
+
+	case 0xEA:
+	    printf("JP PE, addr\n");
+	    break;
+
+	case 0xEB:
+	    printf("XCHG\n");
+	    break;
+
+	case 0xEC:
+	    printf("CPE addr\n");
+	    break;
+
+	case 0xED:
+	    printf("NOP\n");
+	    break;
+
+	case 0xEE:
+	    printf("XRI data\n");
+	    break;
+
+	case 0xEF:
+	    printf("RST 5\n");
+	    break;
+
+	case 0xF0:
+	    printf("RP\n");
+	    break;
+
+	case 0xF1:
+	    printf("POP PSW\n");
+	    break;
+
+	case 0xF2:
+	    printf("JP P, addr\n");
+	    break;
+
+	case 0xF3:
+	    printf("DI\n");
+	    break;
+
+	case 0xF4:
+	    printf("CP addr\n");
+	    break;
+
+	case 0xF5:
+	    printf("PUSH PSW\n");
+	    break;
+
+	case 0xF6:
+	    printf("ORI data\n");
+	    break;
+
+	case 0xF7:
+	    printf("RST 6\n");
+	    break;
+
+	case 0xF8:
+	    printf("RM\n");
+	    break;
+
+	case 0xF9:
+	    printf("SPHL\n");
+	    break;
+
+	case 0xFA:
+	    printf("JP M, addr\n");
+	    break;
+
+	case 0xFB:
+	    printf("EI\n");
+	    break;
+
+	case 0xFC:
+	    printf("CM addr\n");
+	    break;
+
+	case 0xFD:
+	    printf("NOP\n");
+	    break;
+
+	case 0xFE:
+	    printf("CPI data\n");
+	    break;
+
+	case 0xFF:
+	    printf("RST 7\n");
+	    break;
+
+
+
     }
 }
 
