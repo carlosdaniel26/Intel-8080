a7fccd8eadcaa54a360cce744a06acf9087656d8
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-03-15 16:56:31 -0300

    Revert "putting global functions together"
    This reverts commit 597b325bd5e42c9d36bb9d391bcaa5f81d258a9e.

--- include/main.h
+++ include/main.h
@@ -12,8 +12,8 @@ static inline void create_window();
 static inline void create_render();
 static inline void create_texture();
 static inline void init_sdl_screen_buffer();
-static inline void finish_and_free();
 void update_screen();
+static inline void finish_and_free();
 void video_buffer_to_screen(Cpu8080 *cpu);
 
 #endif
