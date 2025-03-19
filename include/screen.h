4e8ab14975590688c70a74a5d7f2eb9038204de8
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-03-18 21:03:15 -0300

    now screen functions have its own files

---
+++ include/screen.h
@@ -0,0 +1,10 @@
+#include <cpu.h>
+
+void create_window();
+void create_render();
+void create_texture();
+void init_sdl_screen_buffer();
+void update_screen();
+void finish_and_free(Cpu8080 *cpu);
+void video_buffer_to_screen(Cpu8080 *cpu);
+void init_screen();
